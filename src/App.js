077a1c0ac668d326503a2a2f64d7daa6da71bc32
Andy Ortiz <andyortiz@Andys-MacBook-Pro.local>
2022-12-20 15:32:56 -0300

    Clear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
+import Carousel from './components/Carousel'
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <header className="App-header">
+      <Carousel/>
+      <ItemListContainer/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,34 +4,27 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import CartWidget from './CartWidget';
 
 const navbar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">Celumanía</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
+                <Nav.Link href="#features">Celulares</Nav.Link>
+                <Nav.Link href="#pricing">Accesorios</Nav.Link>
+                <NavDropdown title="Marcas" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">iPhone</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Samsung</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Xiaomi</NavDropdown.Item>
                 </NavDropdown>
+                <Nav.Link href="#deets">Contacto</Nav.Link>
               </Nav>
               <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
+                <CartWidget/>
               </Nav>
             </Navbar.Collapse>
           </Container>
